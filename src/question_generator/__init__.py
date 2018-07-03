dd62ab5fa00900b81d58bb8337b456c0e575a9ac
井田牧子 <ida@idamakiko-no-iMac-2.local>
2018-07-03 15:05:18 +0900

    追加

---
+++ src/question_generator/__init__.py
@@ -0,0 +1,4 @@
+from .q1_generator import q1_generator
+from .q2_generator import q2_generator
+from .q3_generator import q3_generator
+from .main_generator import q_generator_main
---
+++ src/question_generator/q2_generator.py
@@ -0,0 +1,22 @@
+import random
+
+only_nod_template_with_premise = ["以下の意見に賛成意見が出ています！別の考えの方はいますか？\n　「[<title>]\n<s1>\n<s2>」", \
+                                  "ぜひ、このコメントについて返信をしてください！\n　「[<title>]\n<s1>\n<s2>」"]
+
+
+
+def q2_generator(post, si, thread_title):
+    select_template = random.choice(only_nod_template_with_premise)
+
+    claim_s = post.sentences[si].body
+
+    if si > 1:
+        if post.sentences[si - 1].tag == "PREMISE":
+            premise_s = post.sentences[si - 1].body
+            return select_template.replace("<title>", thread_title).replace("<s1>", premise_s).replace("<s2>", claim_s)
+    elif len(post.sentences) > si + 2:
+        if post.sentences[si + 1].tag == "PREMISE":
+            premise_s = post.sentences[si + 1].body
+            return select_template.replace("<title>", thread_title).replace("<s1>", claim_s).replace("<s2>", premise_s)
+
+    return select_template.replace("<title>", thread_title).replace("<s1>", claim_s).replace("<s2>", "")
